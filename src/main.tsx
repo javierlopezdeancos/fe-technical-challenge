cd4a7699d8c1fea57e0e1405d973d21b4aeb641e
Javier López de Ancos <javier.lopez@ontruck.com>
2025-05-06 20:44:59 +0200

    chore: add an error boundary wrapping the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app.tsx'
+import ErrorBoundaryComponent from './components/error-boundary-component.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundaryComponent fallback={<div>Error</div>}>
+      <App />
+    </ErrorBoundaryComponent>
   </StrictMode>,
 )
