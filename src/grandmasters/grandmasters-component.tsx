c0d64acffb41f56bd4e4916b06d8d6209b612689
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-07 21:55:54 +0200

    feat: add new components to gms component

--- src/grandmasters/grandmasters-component.tsx
+++ src/grandmasters/grandmasters-component.tsx
@@ -3,30 +3,32 @@ import { Link } from '@tanstack/react-router'
 import getGrandMastersApi from './get-grandmasters-api.ts'
 import { type GrandmasterUsername } from './grandmasters.ts'
 import GrandmastersSkeletonComponent from './grandmasters-skeleton-component.tsx'
+import TypographyHeader1Component from '@/components/typography-header-1-component.tsx'
+import TypographyParagraphComponent from '@/components/typography-paragraph-component.tsx'
 
 const grandmastersPromise = getGrandMastersApi();
 
 function GrandmastersPage(): React.ReactElement {
-  const grandmasters = use(grandmastersPromise);
+  const grandmasters = use(grandmastersPromise)
 
   return (
     <main>
-      {!grandmasters ?
-        <>
-          <h1>Grandmasters</h1>
-          <ul>
-            {grandmasters.players.map((player:GrandmasterUsername) => (
-              <li key={player}>
-                <Link to="/$username" params={{ username: player }}>
-                  {player}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </>
-        :
-        <GrandmastersSkeletonComponent />
-      }
+      <div className="flex flex-col gap-10 items-start space-x-4 w-full">
+        <TypographyHeader1Component>Grandmasters</TypographyHeader1Component>
+        {grandmasters ?
+            <ul className="my-0 ml-0 [&>li]:mt-0">
+              {grandmasters.players.map((player:GrandmasterUsername) => (
+                <li key={player}>
+                  <Link to="/$username" params={{ username: player }}>
+                    <TypographyParagraphComponent>{player}</TypographyParagraphComponent>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          :
+          <GrandmastersSkeletonComponent />
+        }
+      </div>
     </main>
   )
 }
--- src/grandmasters/grandmasters-skeleton-component.tsx
+++ src/grandmasters/grandmasters-skeleton-component.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import { SkeletonComponent } from "@/components/skeleton-component"
+import React, { useId } from 'react'
+import SkeletonComponent from "@/components/skeleton-component.tsx"
+import getRandomNumberBetweenLimitsHelper from "@/helpers/get-random-number-between-limits-helper.ts"
+import useViewportHook from "@/hooks/viewport-hook.ts"
 
 function GrandmastersSkeletonComponent(): React.ReactElement {
+  const componentId = useId()
+  const { vh } = useViewportHook()
+
+  const numberOfGrandmasters = vh % 24
+
+  const skeletons =  Array.from({ length: numberOfGrandmasters }, (_, i) => getRandomNumberBetweenLimitsHelper(10, 60) + i)
+
+  console.log(numberOfGrandmasters)
+
   return (
-      <div className="flex items-center space-x-4">
-        <SkeletonComponent className="h-4 w-full" />
+      <div className="w-full flex flex-col gap-4 items-start space-x-4 overflow-hidden">
+        {
+          skeletons.map((s: number) => {
+            return <SkeletonComponent key={`${componentId}-${s}`} className={`h-4`} style={{ width: `${s}%` }} />
+          })
+        }
+
       </div>
   )
 }
