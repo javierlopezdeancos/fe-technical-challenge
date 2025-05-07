a5f419d17e194237a8f2be16c07ec3f6a079c98c
Javier López de Ancos <javier.lopez@ontruck.com>
2025-05-07 15:28:29 +0200

    feat: add skeleton to gms list

--- src/grandmasters/grandmasters-component.tsx
+++ src/grandmasters/grandmasters-component.tsx
@@ -2,6 +2,7 @@ import React, { use } from 'react'
 import { Link } from '@tanstack/react-router'
 import getGrandMastersApi from './get-grandmasters-api.ts'
 import { type GrandmasterUsername } from './grandmasters.ts'
+import GrandmastersSkeletonComponent from './grandmasters-skeleton-component.tsx'
 
 const grandmastersPromise = getGrandMastersApi();
 
@@ -10,16 +11,22 @@ function GrandmastersPage(): React.ReactElement {
 
   return (
     <main>
-      <h1>Grandmasters</h1>
-      <ul>
-        {grandmasters.players.map((player:GrandmasterUsername) => (
-          <li key={player}>
-            <Link to="/$username" params={{ username: player }}>
-              {player}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {!grandmasters ?
+        <>
+          <h1>Grandmasters</h1>
+          <ul>
+            {grandmasters.players.map((player:GrandmasterUsername) => (
+              <li key={player}>
+                <Link to="/$username" params={{ username: player }}>
+                  {player}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+        :
+        <GrandmastersSkeletonComponent />
+      }
     </main>
   )
 }
