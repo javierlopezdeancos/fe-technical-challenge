28d2896594d6e41740d5bd4b1f5041a959341416
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-07 18:24:58 +0200

    chore: remove vite router plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss()
   ],
