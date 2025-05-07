9585aa549940d30a0ae181e4d49290bcb5c47e45
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-07 23:32:27 +0200

    feat: add global app loader

--- src/app-layout.tsx
+++ src/app-layout.tsx
@@ -1,9 +1,20 @@
-import React, { type PropsWithChildren } from 'react'
+import React, { useState, useEffect,  type PropsWithChildren } from 'react'
+import LoaderComponent from "@/components/loader/loader-component.tsx"
 
 function AppLayout({ children }: PropsWithChildren): React.ReactElement {
+  const [iLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false)
+    }, 1500)
+
+    return () => clearTimeout(timeoutId)
+  },[])
+
   return (
     <div className="w-full h-full px-10 py-8">
-      {children}
+      {iLoading ? <LoaderComponent/> : children}
     </div>
   )
 }
