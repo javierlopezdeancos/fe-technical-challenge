eb96c92c8a991c5cc678699b7101377070010766
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-07 23:34:11 +0200

    feat: add avatar and title to gm card

--- src/grandmasters/grandmaster-component.tsx
+++ src/grandmasters/grandmaster-component.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { getRouteApi } from '@tanstack/react-router'
+import AvatarComponent, { AvatarImageComponent, AvatarFallbackComponent } from '@/components/avatar-component.tsx'
+import CardComponent, { CardTitleComponent, CardContentComponent } from '@/components/card-component.tsx'
+import TypographyHeader1Component from '@/components/typography/typography-header-1-component'
+import BadgeComponent from '@/components/badge/badge-component.tsx'
+import SeparatorComponent from '@/components/separator-component.tsx'
 
 function GrandmasterPage(): React.ReactElement | null {
   const grandmasterApi = getRouteApi('/$username')
@@ -7,8 +12,23 @@ function GrandmasterPage(): React.ReactElement | null {
 
   return (
     <main>
-      <h1>Grandmaster</h1>
-      <p>{grandmaster.username}</p>
+      <CardComponent className="w-full px-5">
+        <CardContentComponent className='px-0'>
+          <div className='flex flex-row items-start justify-start gap-6'>
+            <AvatarComponent className="size-24" >
+              <AvatarImageComponent className="size-24" src={grandmaster.avatar} alt="@shadcn" />
+              <AvatarFallbackComponent>CN</AvatarFallbackComponent>
+            </AvatarComponent>
+            <div className='w-full flex flex-col items-start justify-center gap-3'>
+              <CardTitleComponent className='w-auto'><TypographyHeader1Component>{grandmaster.username}</TypographyHeader1Component></CardTitleComponent>
+              <div className='flex flex-row items-center justify-start gap-6'>
+                <BadgeComponent>{grandmaster.title}</BadgeComponent >
+              </div>
+              <SeparatorComponent className='my-5' />
+            </div>
+          </div>
+        </CardContentComponent>
+      </CardComponent>
     </main>
   )
 }
